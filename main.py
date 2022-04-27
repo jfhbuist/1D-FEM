4d52e544634efae3c1357daf2b304c52e8df8225
Jurriaan Buist <jurriaanbu@gmail.com>
2022-04-27 13:26:58 +0200

    Changed line indents to comply with PEP 8

--- main.py
+++ main.py
@@ -20,60 +20,60 @@ d_i = \int_0^1 f*phi_i dx
 """
 
 import numpy as np
-from scipy.integrate import quad
+# from scipy.integrate import quad
 import matplotlib.pyplot as plt
 import fem_solver as fem
 
 
 def plot_solution(x, u, n, D, a, alpha, beta, gamma):
-  plt.plot(x,u)
-  plt.xlim(0,1)
-  plt.xlabel('x')
-  plt.ylabel('u')
-  title = r'n={:d}, D={:.1f}, a={:.1f}, $\alpha$={:.1f}, $\beta$={:.1f}, $\gamma$={:.1f}'.format(n,D,a, alpha, beta, gamma)
-  plt.title(title, y=1.02, fontsize = 16)
-  plt.grid()
-  plt.show()
+    plt.plot(x,u)
+    plt.xlim(0,1)
+    plt.xlabel('x')
+    plt.ylabel('u')
+    title = r'n={:d}, D={:.1f}, a={:.1f}, $\alpha$={:.1f}, $\beta$={:.1f}, $\gamma$={:.1f}'.format(n,D,a, alpha, beta, gamma)
+    plt.title(title, y=1.02, fontsize = 16)
+    plt.grid()
+    plt.show()
   
 def main():
 
-  ## Input
+    ## Input
   
-  # Reference input
-  n = 5
-  D = 1
-  a = 0.8
-  alpha = 0.5
-  beta = 2
-  gamma = 30
+    # Reference input
+    n = 5
+    D = 1
+    a = 0.8
+    alpha = 0.5
+    beta = 2
+    gamma = 30
   
-  # n = 100
-  # D = 1
-  # a = 1
-  # alpha = 0
-  # beta = 1
-  # gamma = 20
+    # n = 100
+    # D = 1
+    # a = 1
+    # alpha = 0
+    # beta = 1
+    # gamma = 20
   
-  grid = fem.Grid(n)
+    grid = fem.Grid(n)
   
-  source = fem.Source(grid, alpha, beta, gamma)
-  print(source.d)
+    source = fem.Source(grid, alpha, beta, gamma)
+    print(source.d)
     
-  diffusion = fem.Diffusion(grid, D)
-  print(diffusion.s_D)
+    diffusion = fem.Diffusion(grid, D)
+    print(diffusion.s_D)
   
-  advection = fem.Advection(grid, a)
-  print(advection.s_A)
+    advection = fem.Advection(grid, a)
+    print(advection.s_A)
   
-  stiffness = fem.StiffnessMatrix(grid,[diffusion.s_D,advection.s_A])
-  print(stiffness.s)
+    stiffness = fem.StiffnessMatrix(grid,[diffusion.s_D,advection.s_A])
+    print(stiffness.s)
   
-  u = np.linalg.solve(stiffness.s,source.d) # solve for solution values at vertices
-  print(u)
+    u = np.linalg.solve(stiffness.s,source.d) # solve for solution values at vertices
+    print(u)
   
-  plot_solution(grid.x_vert, u, n, D, a, alpha, beta, gamma)
+    plot_solution(grid.x_vert, u, n, D, a, alpha, beta, gamma)
       
 if __name__=='__main__':
-  main()
+    main()
   
 
